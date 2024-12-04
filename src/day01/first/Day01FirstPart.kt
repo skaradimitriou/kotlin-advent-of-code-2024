cd2bf96556ae04e6398fedc043100c86e5167109
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2024-12-04 10:23:46 +0200

    Impl: Day01 challenges

---
+++ src/day01/first/Day01FirstPart.kt
@@ -0,0 +1,34 @@
+package day01.first
+
+import readInput
+
+const val TEST_DATA = "day01/day01_test_data"
+const val ACTUAL_DATA = "day01/day01_actual_data"
+
+fun main() {
+    println("DAY 03 Challenge (First Part)...")
+    println("-------")
+
+    val input = readInput(ACTUAL_DATA).map {
+        it.trim().split("\\s+".toRegex())
+    }
+
+    val firstColumn = input.map { it[0] }.sorted()
+    val secondColumn = input.map { it[1] }.sorted()
+
+    val totalDistance = firstColumn.zip(secondColumn).sumOf { pair ->
+        val first = pair.first.toInt()
+        val second = pair.second.toInt()
+
+        when {
+            first > second -> first - second
+            else -> second - first
+        }
+    }
+
+    println(input)
+    println("First col : $firstColumn")
+    println("Second col : $secondColumn")
+
+    println("Distances: $totalDistance")
+}
---
+++ src/day01/second/Day01SecondPart.kt
@@ -0,0 +1,55 @@
+package day01.second
+
+import readInput
+
+const val TEST_DATA = "day01/day01_test_data"
+const val ACTUAL_DATA = "day01/day01_actual_data"
+
+const val TEST_SOLUTION = 31
+const val ACTUAL_SOLUTION = 23177084
+
+fun main() {
+    println("DAY 03 Challenge (Second Part)...")
+    println("-------")
+
+    val testSimilarityScore = readInput(TEST_DATA).divideIntoColumns().getTotalSimilarityScore()
+    check(testSimilarityScore == TEST_SOLUTION)
+
+    val actualSimilarityScore = readInput(ACTUAL_DATA).divideIntoColumns().getTotalSimilarityScore()
+    check(actualSimilarityScore == ACTUAL_SOLUTION)
+}
+
+fun List<String>.divideIntoColumns(): List<List<String>> = map {
+    it.trim().split("\\s+".toRegex())
+}
+
+fun List<List<String>>.getTotalSimilarityScore(): Int {
+    val firstColumn = map { it[0] }
+    val secondColumn = map { it[1] }
+
+    println("Original Input => $this")
+    println("-------")
+    println("First col : $firstColumn")
+    println("Second col : $secondColumn")
+    println("-------")
+
+    val totalSimilarityScore = List(secondColumn.size) { index ->
+        try {
+            val valueFromFirstColumn = firstColumn[index].toInt()
+            val occurrences = secondColumn.map { it.toInt() }
+                .count { it == valueFromFirstColumn }
+
+            val similarityScore = valueFromFirstColumn.times(occurrences)
+
+            println("Number: $valueFromFirstColumn appears $occurrences times. Similarity Score : $similarityScore")
+
+            similarityScore
+        } catch (e: Exception) {
+            error("Lists don't have the same size.")
+        }
+    }.sum()
+
+    println("-------")
+    println("The total similarity score is $totalSimilarityScore.")
+    return totalSimilarityScore
+}
